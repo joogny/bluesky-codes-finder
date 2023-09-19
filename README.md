26e8c87954927a51002b3157a64fba1defc8c95b
Joogny <millourj@gmail.com>
2023-09-19 22:01:00 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+This python script finds bluesky codes posted on twitter.
+Every 30 seconds, the script will redo the request and print newly found invite codes on the terminal.
+
+# How to use
+
+1. Add you twitter username and password to the .env file (USERNAME and PASSWORD)
+2. Run the python script
+
+Every 30 codes, newly posted bluesky invite codes will be printed.
---
+++ get_codes.py
@@ -0,0 +1,29 @@
+from tweety import Twitter
+import os
+from dotenv import load_dotenv
+from tweety.filters import SearchFilters
+import re
+import time
+
+load_dotenv()
+username = os.getenv('USERNAME')
+password = os.getenv('PASSWORD')
+app = Twitter("session")
+app.sign_in(username, password)
+
+codes = []
+# pattern to find in tweet text
+pattern = r'\bbsky-social-\w+\b'
+
+
+print("Here are the bluesky codes found: (will be updated every minute):")
+while (True):
+    # can't remove bsky.social.* username from searches :(
+    tweets = app.search("'bsky-social-'", filter_=SearchFilters.Latest())
+    for tweet in tweets:
+        matches = re.findall(pattern, tweet.text)
+        for match in matches:
+            if (match not in codes):
+                print(match)
+                codes.append(match)
+    time.sleep(30)
